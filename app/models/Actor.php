0765987e59e608b7e6a51faa346bb8df1bd75458
RubenSans82 <rubensans82@gmail.com>
2025-02-24 17:39:18 +0100

    inicio

---
+++ app/models/Actor.php
@@ -0,0 +1,10 @@
+<?php
+namespace Formacom\Models;
+use Illuminate\Database\Eloquent\Model;
+class Actor extends Model{
+    protected $table="actor";
+    protected $primaryKey = 'actor_id';
+
+}
+
+?>
